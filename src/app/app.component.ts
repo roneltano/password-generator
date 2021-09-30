90329aa7a6d6ebf151ad137aa012a27f61d56ce1
Ronell John Tano <ronel.tano98@gmail.com>
2021-09-30 12:32:52 +0800

    UpdatedCodes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,63 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password-generator';
+  password = '';
+  title = 'Password Generator';
+  pwlength = 0;
+  useLetters = false;
+  useSymbols = false;
+  useNumbers = false;
+
+  onSubmit() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.useLetters) {
+      validChars += letters;
+    }
+
+    if (this.useNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.useSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.pwlength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  getLength(value: string) {
+    const parsedInt = parseInt(value);
+
+    if (!isNaN(parsedInt)) {
+      this.pwlength = parsedInt;
+      console.log(this.pwlength);
+    }
+  }
+
+  includesLetters() {
+    this.useLetters = !this.useLetters;
+    console.log(this.useLetters);
+  }
+  includesNumbers() {
+    this.useNumbers = !this.useNumbers;
+    console.log(this.useNumbers);
+  }
+  includesSymbols() {
+    this.useSymbols = !this.useSymbols;
+    console.log(this.useSymbols);
+  }
 }
